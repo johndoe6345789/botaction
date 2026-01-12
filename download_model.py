5bd32795710b8554aa9d1b0d67dab36495321861
johndoe6345789 <john.doe6345789@gmail.com>
2026-01-12 18:50:05 +0000

    Update model URL in download_model.py for fetching specific Sketchfab model

--- download_model.py
+++ download_model.py
@@ -4,7 +4,7 @@ from src.export_stl import ModelSTLExporter
 
 # 1. Fetch
 fetcher = SketchfabFetcher()
-result = fetcher.fetch_model("https://sketchfab.com/3d-models/...")
+result = fetcher.fetch_model("https://sketchfab.com/3d-models/annihilator-2000-dea4f17e94974e1fa720cbadc531ed63")
 fetcher.download_model_files(result['model_id'], 'downloads')
 
 # 2. Decrypt + Decode + Export
